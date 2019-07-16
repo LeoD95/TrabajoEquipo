11c81cabdc376b369703639de3aab3ba91347c81
LeoD95 <gary.reyes98@gmail.com>
2019-07-15 19:24:26 -0500

    Se agrego el modulo de sucursales

---
+++ inicio-Neto/panel.php
@@ -0,0 +1,84 @@
+<div class="row">
+	<div class="container-fluid">
+
+		<div class="panel-group" id="accordion" role="tablist">
+			<div class="panel panel-color">
+				<div class="panel-heading" role="tab" id="heading1">
+					<h4 class="panel-title">
+						<a href="#collapse1" data-toggle="collapse" data-parent="#accordion">
+							Administración del sistema.
+						</a>				
+					</h4>
+				</div>
+
+				<div id="collapse1" class="panel-collapse collapse in">
+					<div class="panel-body">
+
+						<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+							<div class="caja">
+								<i class="fas fa-user-friends"></i>
+								<p>
+									<a href="../mPersonas/index.php">Personas</a>
+								</p>
+							</div>
+						</div>
+
+						<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+							<div class="caja">
+								<i class="fas fa-users-cog"></i>
+								<p>
+									<a href="../mUsuarios/index.php">Usuarios</a>
+								</p>
+							</div>
+						</div>
+
+						<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+							<div class="caja">
+								<i class="fas fa-camera"></i>
+								<p>
+									<a href="../mFotografiaUsuarios/index.php">Fotografía de usuarios</a>
+								</p>
+							</div>
+						</div>
+
+					</div>
+				</div>				
+			</div>
+		</div>
+
+		<div class="panel-group" id="accordion" role="tablist">
+			<div class="panel panel-color">
+				<div class="panel-heading" role="tab" id="heading1">
+					<h4 class="panel-title">
+						<a href="#collapse2" data-toggle="collapse" data-parent="#accordion">
+							Categoria de modulo.
+						</a>				
+					</h4>
+				</div>
+
+				<div id="collapse2" class="panel-collapse collapse ">
+					<div class="panel-body">
+						<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+							<div class="caja">
+								<i class="fa fa-graduation-cap"></i>
+								<p>
+									<a href="../mAlumnos/index.php">Módulo de Alumnos</a>
+								</p>
+							</div>
+						</div> 
+						<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+							<div class="caja">
+								<i class="fa fa-bookmark"></i>
+								<p>
+									<a href="../mAlumnos/index.php">Módulo de Carreras</a>
+								</p>
+							</div>
+						</div> 
+					</div>
+				</div>				
+			</div>
+		</div>
+
+	</div>					
+</div>
+	
